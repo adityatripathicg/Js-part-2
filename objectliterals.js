f5788afc9964d61ccddbf060b7f165187924dbd8
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-09 23:24:57 +0530

    twitter post

--- objectliterals.js
+++ objectliterals.js
@@ -3,3 +3,10 @@ let student = {
     age : 20,
     marks : 99.9
 };
+ const post = {
+    username: "@Adityaxcg",
+    content : "This is my first post",
+    likes : 150,
+    reposts : 4,
+    tags : ["@shrutiagrahari","@adityaxcg"]
+ };
