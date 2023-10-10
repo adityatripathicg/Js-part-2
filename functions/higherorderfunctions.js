89a992e14362be1380fd468d3c9846248ff33a68
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-10 21:08:44 +0530

    returns a function

--- functions/higherorderfunctions.js
+++ functions/higherorderfunctions.js
@@ -7,3 +7,24 @@ let greet = function(){
     console.log("Hello!");
 }
 multiplegreet(greet, 5);
+// higher order function which returns a function
+function oddeventest(req) {
+    if(req == "odd"){
+        let odd = function (n){
+            console.log(!(n%2==0));
+        }
+        return odd;
+        
+    }
+    else if(req == "even"){
+        let even = function (n){
+            console.log((n%2==0));
+        }
+        return even;
+    }
+    else{
+        console.log("Wrong Request!");
+    }
+}
+let req = "odd";
+let func = (oddeventest(req));
