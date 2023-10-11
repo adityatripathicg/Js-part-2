a9c35ec689e867f85df5837ada76e2ec3ae525e1
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-11 21:27:01 +0530

    this.age

--- this.js
+++ this.js
@@ -4,8 +4,11 @@ const student = {
     eng : 99,
     math : 90,
     phy : 90,
+    ages(){
+        console.log(`${this.name}'s age is ${this.age}.`)
+    },
     getavg(){
         let avg = (this.eng+this.phy+this.math)/3;
         console.log(`${this.name} got avg of ${avg}.`);
     }
-}
+};
