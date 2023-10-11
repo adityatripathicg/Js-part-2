7b61f9c1dc67c35bafd2caa5751750357724b4f6
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-12 00:35:46 +0530

    set Interval

--- timeout.js
+++ timeout.js
@@ -3,3 +3,9 @@ setTimeout(()=>{
 },4000); // time in ms
 console.log("Hello");
 setTimeout(()=>{console.log("How are you...?");},8000);
+
+//setInterval
+let id = setInterval(() => {
+    console.log("SCG CREATIONS");
+}, 5000);
+clearInterval(id);
