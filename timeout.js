0ab24e03f9f06bf86561526d72fefce2c67d8baf
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-12 00:42:29 +0530

    SetInterval which prints HW in 2 secs 5 times

--- timeout.js
+++ timeout.js
@@ -12,4 +12,11 @@ clearInterval(id);
 
 //arrow function that returns square of N
 let sq = ((n)=>n*n);
-//
+//SetInterval which prints HW in 2 secs 5 times
+
+let times = setInterval(() => {
+    console.log("Hello World!");
+}, 2000);
+setTimeout(() => {
+    clearInterval(times)
+}, 10000);
