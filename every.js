fa3fa6c5f7ea74ab338a3cf183d6e8bd3496f4fb
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-13 06:45:27 +0530

    reduce array methods

--- every.js
+++ every.js
@@ -1,2 +1,4 @@
 let even = [2,4,6,8,10];
 console.log(even.every((num) => num%2==0));//return true if every element in array returns true to the function else false
+//reduce array methods
+console.log(even.reduce((result,element) => result+element));
