1d86bb79823e89cd315e0947884af786ea5723c7
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-26 23:53:30 +0530

    new operator better then factory function

--- ass17.js
+++ ass17.js
@@ -9,3 +9,13 @@ console.log(arr);
 //2
 let n = 12345;
 let count = 0;
+// new operator better then factory function
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.talk = function(){
+    console.log(`I am ${this.username}`);
+};
+let p1 = new Person("Aditya", 20);
+let p2 = new Person("SCG", 20);
